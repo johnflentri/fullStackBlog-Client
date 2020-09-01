dd427e9d64d9053d0856c599e3ca8d75f9d12ad5
johnflentri <jpflentri@gmail.com>
2020-09-01 14:56:49 +0200

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios'
+
+const axiosInstance = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com'
+})
+
+class App extends Component {
+  state = {
+    hello: null
+  }
+
+  componentDidMount() {
+    axiosInstance.get('/posts')
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err))
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.hello
+          ? <div> {this.state.hello} </div>
+          : null}
+      </div>
+    );
+  }
+}
+
+export default App;
